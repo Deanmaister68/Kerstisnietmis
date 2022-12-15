294efe10ba3a49ee7e9d4d43fbed848d27971e58
Bushman-31 <513323@vistacollege.nl>
2022-12-15 12:32:21 +0100

    text colors added
    we kunnen nu text kleuren toevoegen.

--- barkeeper.py
+++ barkeeper.py
@@ -1,4 +1,9 @@
-print("Barkeeper: Welcome to the Frozen Casket traveller. How can I be of service?")
+#player name input in file is voor testing. ER UIT HALEN BIJ HET AFMAKEN VAN FILE!!!
+Input_playername = ("dave")
+
+from colorama import Fore, Back, Style
+
+print(Fore.GREEN + "Barkeeper: Welcome to the Frozen Casket traveller. How can I be of service?")
 
 drinks = input("Barkeeper: Would you like an ale? ")
 if drinks == "yes":
@@ -8,4 +13,4 @@ else:
     
 print("Barkeeper: Well traveller, what is you're story?")
 
-print(Input_playername + ": One day I was going for a walk in the woods till I heard a scream  coming from the house where me and hmy family lived. When I came back, I saw a horrific sight of my wife and children slaughtert by the orders of the evil king. Filled with rage I picked up an iron sword my father left me and started my quest of revenge. ")
+print(Fore.YELLOW + Input_playername + ": One day I was going for a walk in the woods till I heard a scream  coming from the house where me and hmy family lived. When I came back, I saw a horrific sight of my wife and children slaughtert by the orders of the evil king. Filled with rage I picked up an iron sword my father left me and started my quest of revenge. ")
