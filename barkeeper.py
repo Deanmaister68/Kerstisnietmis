6651466a91f13447772cc15489bf4f3563696dc3
Bushman-31 <513323@vistacollege.nl>
2022-12-15 14:11:16 +0100

    laatste update barkeeper npc
    barkeeper npc is af

--- barkeeper.py
+++ barkeeper.py
@@ -1,6 +1,3 @@
-#player name input in file is voor testing. ER UIT HALEN BIJ HET AFMAKEN VAN FILE!!!
-Input_playername = ("dave")
-
 from colorama import Fore, Back, Style
 
 print(Fore.BLUE + "Barkeeper:" + Fore.WHITE + " Welcome to the Frozen Casket traveller. How can I be of service?")
@@ -13,8 +10,12 @@ else:
     
 print(Fore.BLUE + "Barkeeper:" + Fore.WHITE + " Well traveller, what is you're story?")
 
-print("\n" + Fore.YELLOW + Input_playername + Fore.WHITE +  ": One day I was going for a walk in the woods till I heard a scream  coming from the house where me and hmy family lived. When I came back, I saw a horrific sight of my wife and children slaughtert by the orders of the evil king. Filled with rage I picked up an iron sword my father left me and started my quest of revenge. ")
+print("\n" + Fore.YELLOW + Input_playername + Fore.WHITE +  ": One day I was going for a walk in the woods till I heard a scream  coming from the house where me and my family lived. When I came back, I saw a horrific sight of my wife and children slaughtert by the orders of the evil king. Filled with rage I picked up an iron sword my father left me and started my quest of revenge. ")
 
 barkeepgift = input("\n" + Fore.BLUE + "Barkeeper:" + Fore.WHITE + "Im sorry for your los traveller. may i give you a gift? " + Fore.CYAN + "\nanswer ")
 if barkeepgift == "yes":
     print(Fore.MAGENTA + "narrator:" + Fore.WHITE + " you received an healing item" + Fore.GREEN + "(green goo) " + Fore.WHITE + " use it to regenerate +5 procent of health")
+else:
+    print("I geus i will keep this healing item for myself than")
+
+print(Fore.MAGENTA + "narrator:" + Fore.WHITE + "After you thank the barkeeper you move on with you're quest")
