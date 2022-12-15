66c8f32f2333bc889d049f6cfe1808ac1a0fe73c
Bushman-31 <513323@vistacollege.nl>
2022-12-15 13:25:58 +0100

    npc update
    ik heb de text kleuren en de text structuur aangepast

--- barkeeper.py
+++ barkeeper.py
@@ -3,14 +3,18 @@ Input_playername = ("dave")
 
 from colorama import Fore, Back, Style
 
-print(Fore.GREEN + "Barkeeper: Welcome to the Frozen Casket traveller. How can I be of service?")
+print(Fore.BLUE + "Barkeeper:" + Fore.WHITE + " Welcome to the Frozen Casket traveller. How can I be of service?")
 
-drinks = input("Barkeeper: Would you like an ale? ")
+drinks = input(Fore.BLUE + "Barkeeper:" + Fore.WHITE + " Would you like an ale? " + Fore.CYAN + "\nanswer ")
 if drinks == "yes":
-    print("Barkeeper: Here you go!")
+    print("\n" + Fore.BLUE + "Barkeeper:" + Fore.WHITE + " Here you go!")
 else:
-    print("Barkeeper: Not someone who likes a nice ale are ya.")
+    print("\n" + Fore.BLUE + "Barkeeper:" + Fore.WHITE + " Not someone who likes a nice ale are ya.")
     
-print("Barkeeper: Well traveller, what is you're story?")
+print(Fore.BLUE + "Barkeeper:" + Fore.WHITE + " Well traveller, what is you're story?")
 
-print(Fore.YELLOW + Input_playername + ": One day I was going for a walk in the woods till I heard a scream  coming from the house where me and hmy family lived. When I came back, I saw a horrific sight of my wife and children slaughtert by the orders of the evil king. Filled with rage I picked up an iron sword my father left me and started my quest of revenge. ")
+print("\n" + Fore.YELLOW + Input_playername + Fore.WHITE +  ": One day I was going for a walk in the woods till I heard a scream  coming from the house where me and hmy family lived. When I came back, I saw a horrific sight of my wife and children slaughtert by the orders of the evil king. Filled with rage I picked up an iron sword my father left me and started my quest of revenge. ")
+
+barkeepgift = input("\n" + Fore.BLUE + "Barkeeper:" + Fore.WHITE + "Im sorry for your los traveller. may i give you a gift? " + Fore.CYAN + "\nanswer ")
+if barkeepgift == "yes":
+    print(Fore.MAGENTA + "narrator:" + Fore.WHITE + " you received an healing item" + Fore.GREEN + "(green goo) " + Fore.WHITE + " use it to regenerate +5 procent of health")
