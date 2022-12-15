520d5ee89d3de553e2e25eac232ef302a94a3c18
Bushman-31 <513323@vistacollege.nl>
2022-12-15 11:17:08 +0100

    barkeeper ale option
    de optie om te drinken voor dat je je verhaal vertelt

---
+++ barkeeper.py
@@ -0,0 +1,9 @@
+print("Barkeeper: Welcome to the Frozen Casket traveller. How can I be of service?")
+
+drinks = input("Barkeeper: Would you like an ale? ")
+if drinks == "yes":
+    print("Barkeeper: Here you go!")
+else:
+    print("Barkeeper: Not someone who likes a nice ale are ya.")
+    
+print("Barkeeper: Well traveller, what is you're story?")
