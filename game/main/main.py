7083869a5c93091282bb19d068d095d4197ab94b
Bushman-31 <513323@vistacollege.nl>
2022-12-16 21:40:16 +0100

    name register
    het begin van het verhaal waar je je naam invult

--- game/main/main.py
+++ game/main/main.py
@@ -1,4 +1,7 @@
 from colorama import Fore, Back, Style
 
-print (Fore.CYAN + "???" "What is you're name traveller?")
-Input_playername = input ("")
+print (Fore.CYAN + "???:" " What is you're name traveller?")
+Input_playername = input ( Fore.CYAN + "\nanswer ")
+
+print ( Fore.CYAN + "???: " "HAHAHA. " + Input_playername + "? so it is you? I expect great things from you peasant.")
+print ("???: " + " you")
