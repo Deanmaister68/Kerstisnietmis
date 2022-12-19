8679b471f8e421f0a25af95a0c44c38bf462cd9d
Bushman-31 <513323@vistacollege.nl>
2022-12-19 10:42:54 +0100

    update
    update

--- game/main/main.py
+++ game/main/main.py
@@ -4,4 +4,4 @@ print (Fore.CYAN + "???:" " What is you're name traveller?")
 Input_playername = input ( Fore.CYAN + "\nanswer ")
 
 print ( Fore.CYAN + "???: " "HAHAHA. " + Input_playername + "? so it is you? I expect great things from you peasant.")
-print ("???: " + " you")
+print ( Fore.CYAN + "???: " + " you")
