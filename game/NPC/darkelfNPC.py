5e386a51c2e4e47cbde2eb5a2f0f26ecba4944dd
Bushman-31 <513323@vistacollege.nl>
2022-12-20 14:14:55 +0100

    npc update

---
+++ game/NPC/darkelfNPC.py
@@ -0,0 +1,2 @@
+print ("narator: ""you see a pair of glowing eyes in the shadows next to the church. no matter where you move they keep following you.\nyou make the choice to run and while looking back if it follows you. BOOM! \nyou run in to something. when you look up you see a Dark elf with white glowing eyes standing right in front of you.")
+print ("???: " "you will have to kill the Dark elf if you want to keep on going towards you're revenge. kill it!")
---
+++ game/NPC/jackfrost.py
@@ -0,0 +1,2 @@
+print ("narator: " "there is a snow blizzard appearing around you in fast pace. and you see a schadow of a snowman appear infront of you.\n when you get closer you see its one of the worst entities you could encounter at this moment of time. its jack frost")
+print ("???: " "melt him till only the sticks of his brow are left in the snow! now!")
